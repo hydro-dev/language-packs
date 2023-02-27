3e6074ceba01975534758a3fd1391d96c2dc0c10
undefined <i@undefined.moe>
2023-02-27 21:02:12 +0800

    update

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,20 @@
 import { yaml, fs } from '@hydrooj/utils';
 
+const english = yaml.load(fs.readFileSync('en.yaml', 'utf8'));
+const chinese = yaml.load(fs.readFileSync('zh.yaml', 'utf8'));
+delete english.__id;
+delete chinese.__id;
+delete english.__langname;
+delete chinese.__langname;
+const keys = Object.keys(chinese);
+for (const key of keys) {
+    if (english[key] === undefined) {
+        english[key] = key;
+    }
+}
+const entries = Object.fromEntries(Object.entries(english).sort((a, b) => a[0].localeCompare(b[0])));
+fs.writeFileSync('en.yaml', yaml.dump(entries));
+
 fs.ensureDirSync('dist');
 const files = fs.readdirSync('locales');
 for (const file of files) {
